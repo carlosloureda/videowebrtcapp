ec572d5abfd70cda4235203b4cc54809f7eee965
Carlos <carloslouredaparrado@gmail.com>
2020-08-08 13:34:04 +0200

    feat: Socket io added (user connected working)

--- server.js
+++ server.js
@@ -8,7 +8,20 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+// run anytime a user connects
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    console.log(roomId, userId);
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+  });
 });
 
 server.listen(3000);
