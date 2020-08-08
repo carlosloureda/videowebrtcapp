c0f658e6c8de0b575fc8391046925cc8e2b4c555
Carlos <carloslouredaparrado@gmail.com>
2020-08-08 13:08:49 +0200

    feat: Initial server config

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+server.listen(3000);
